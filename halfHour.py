18a1c197d1fff5c9671050432b2905e0fbe5120d
aboukema <aboukema@avignon.studlab.os3.nl>
2017-06-16 11:10:08 +0200

    transformed data to half an hour over 3 days

--- halfHour.py
+++ halfHour.py
@@ -20,7 +20,7 @@ def init_list(x):
     return empty_array
 
 
-def read_rrd(key):
+def read_rrd(key, interval):
     global tot
     a = 0
     os.chdir(pwd)
@@ -31,15 +31,36 @@ def read_rrd(key):
                 '-r', '6m', '--start', t1, '--end', t2)
        #   qq print filename
         if a == 0:
-            print "length of file = ", len(file[2])
+            #print "length of file = ", len(file[2])
             a = init_list(len(file[2]))
             b = init_list(len(file[2]))
             empty = 1
+             
         a, b = sum_lists(file, a, b)
         tot += 1
         #print "\n empty indexes", filename,  b
-    return a, b
 
+    half = make_half(a, interval)
+    print half
+    return half, b
+
+
+def make_half(a, interval):
+    length = len(a)/interval +1
+    half = init_list(length)
+    new_interval = 0
+    count = 1
+    for i in range(0, len(a)):
+        if count != interval:
+            half[new_interval] += a[i]
+            count += 1
+        else:
+            half[new_interval] = half[new_interval]/interval
+            new_interval += 1
+            count = 1
+    print "length of file = ", len(half)
+    return half
+  
 
 def sum_lists(a, b, empty_values = 0):
     a = list(a)   
@@ -48,6 +69,7 @@ def sum_lists(a, b, empty_values = 0):
     for i in range(0, len(a[2])):
         if isinstance(a[2][i][0], float): #eigenlijk moet bij een none het hele datapunt worden verwijderd
             b[i] += a[2][i][0]
+
         else:
             empty_values[i] += 1
     return b, empty_values 
@@ -59,12 +81,12 @@ def sum_lists1(a, b):
     return l 
 
 
-cpu, empty_cpu = read_rrd('*cpu_user*')
-mem, empty_mem = read_rrd('*total*')
-watt, empty_watt = read_rrd('hw_*')
-print cpu, empty_cpu, tot
-print mem, empty_mem
-print watt
+cpu, empty_cpu = read_rrd('*cpu_user*',5)
+mem, empty_mem = read_rrd('*total*',5)
+watt, empty_watt = read_rrd('hw_*',6)
+#print cpu, empty_cpu, tot
+#print mem, empty_mem
+#print watt
 
 os.chdir('/home/aboukema/rp2/data/git')
 
