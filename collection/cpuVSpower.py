f009b9de69f856284bdf577244f7040aa80ace54
aboukema <aboukema@avignon.studlab.os3.nl>
2017-06-29 17:00:34 +0200

    mem = addition total/cache/buffer

--- collection/cpuVSpower.py
+++ collection/cpuVSpower.py
@@ -37,7 +37,7 @@ def read_rrd(key, interval, pwd = pwd1):
 
         for filename in file_list: #--- Itterate through all files in directory ---#
 #            if pwd == pwd1: #--- uncomment if you want to TUNE the data in pwd1---#
-#                rrdtool.tune('%s/%s' % (pwd,filename),'DELRRA:2')#'RRA#0:+4320')# )   ###  # add extra fill up first RRA with 4320 data points
+#                rrdtool.tune('%s/%s' % (pwd,filename),'DELRRA:2')#'RRA#0:+4320')## )   ###  # add extra fill up first RRA with 4320 data points
             rrdfile = rrdtool.fetch( '%s/%s' % (pwd,filename), 'AVERAGE',
                '-r', '5m', '--start', t1, '--end', t2)
             match =".*%i.*" %i
@@ -153,8 +153,12 @@ def gen_indexes(e1, l1, e2, l2 , e3 = [], l3 = []):
 cpu_system, e1 = read_rrd('*cpu_system*',5)
 cpu_softirq, e2 = read_rrd('*cpu_softirq*',5)
 cpu_user, e3 = read_rrd('*cpu_user*',5)
-cpu_idle, e11 = read_rrd('*cpu_idle*',5)
-mem_free, e4 = read_rrd('*mem_free*',5)
+cpu_idle, e4 = read_rrd('*cpu_idle*',5)
+mem_free, e5 = read_rrd('*mem_free*',5)
+mem_cache, e6 = read_rrd('*mem_cache*',5)
+mem_buffer, e7 = read_rrd('*mem_buffer*',5)
+mem_total, e8 = read_rrd('*mem_total*',5)
+
 watt, e5 = read_rrd('hw*',5, pwd2)
 #cpu_hn, e4= read_rrd('hn*_cpu.rrd',6,pwd2)
 #cpu_pack, e6 = read_rrd('usage*',1,pwd3)
@@ -163,14 +167,20 @@ watt, e5 = read_rrd('hw*',5, pwd2)
 #--- Add lists together ---#
 cpu_hw = sum_lists(cpu_system, cpu_softirq)
 cpu_hw = sum_lists(cpu_hw, cpu_user)
-e_hw = sum_lists(e1, e2)
-e_hw = sum_lists(e_hw, e3)
+mem_hw = sum_lists(mem_cache, mem_buffer)
+mem_hw = sum_lists(mem_hw, mem_total)
+
+e_cpu_hw = sum_lists(e1, e2)
+e_cpu_hw = sum_lists(e_cpu_hw, e3)
+
+e_mem_hw = sum_lists(e6,e7)
+e_mem_hw = sum_lists(e_mem_hw,e8)
 #cpu_vis = sum_lists(cpu_hn, cpu_vps)
 #e_vis = sum_lists(e4, e5)
 
 #--- remove the data points which contain 1 or more empty values---#
-print "length before ", len(cpu_hw), len(watt), len(e_hw)
-gen_indexes( e5, watt, e_hw, cpu_hw,e4, mem_free)
+print "length before ", len(cpu_hw), len(watt), len(e_mem_hw)
+gen_indexes( e5, watt, e_cpu_hw, cpu_hw,e_mem_hw, mem_hw)
 print "length after ", len(cpu_hw)
 
 
@@ -179,5 +189,5 @@ used_files = (total_files - not_used_files)
 percent = (float(used_files) /float(total_files))*100
 print "all files: \t", total_files, "\n removed files \t", not_used_files , "\ngives \t%.2f "  %percent, "% useable files"
 #np.save('cpu_', (cpu_vis))
-np.save('cpu_hw', (cpu_hw, mem_free))
+np.save('cpu_mem_hw', (cpu_hw, mem_hw))
 np.save('power_hw', (watt))
